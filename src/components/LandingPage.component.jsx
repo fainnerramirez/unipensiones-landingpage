59e41cdbd597c8921144d0c3dfc92461a533dd71
Fainner Ramirez <framirez@godoycordoba.com>
2023-08-20 23:29:38 -0500

    add compoennt Hero

--- src/components/LandingPage.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LandingPage = () => {
-  return (
-    <div>LandingPage component</div>
-  )
-}
-
-export default LandingPage;
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from '../components/Hero.component'
 
 const Home = () => {
   return (
-    <div>Home page</div>
+    <>
+      <Hero />
+    </>
   )
 }
 
