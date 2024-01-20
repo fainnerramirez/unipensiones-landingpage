40e688edafb9f9992dda2ccf6ad4322959659620
Fainner Ramirez <framirez@godoycordoba.com>
2024-01-19 19:58:15 -0500

    update commit

--- src/components/ModalStartHero.component.jsx
+++ src/components/ModalStartHero.component.jsx
@@ -10,9 +10,9 @@ import {
     ModalBody,
     ModalCloseButton,
     ModalContent,
-    ModalHeader, ModalOverlay, Stack, useDisclosure
+    ModalHeader, ModalOverlay, Stack, useDisclosure, useToast
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineArrowRight } from "react-icons/ai";
 import anfitrion from "../assets/anfitrion.jpg";
 import student from "../assets/estudiante.jpg";
@@ -20,7 +20,9 @@ import logo from "../assets/logoup.png";
 
 const ModalStartHero = () => {
 
+    const [anfitrionesURL, setAnfitrionesURL] = useState('https://anfitriones.unipensiones.app/')
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const toast = useToast();
 
     return (
         <>
@@ -39,10 +41,10 @@ const ModalStartHero = () => {
                 <ModalOverlay />
                 <ModalContent>
                     <Box display={'flex'} justifyContent={'center'} alignItems={'center'} mt={5}>
-                        <Image src={logo} width={100} height={100} borderRadius={5}/>
+                        <Image src={logo} width={100} height={100} borderRadius={5} />
                     </Box>
                     <ModalHeader textAlign={'center'}>¿Como quieres utilizar Unipensiones?</ModalHeader>
-                    <ModalCloseButton _hover={{bg:"black", color: "white"}}/>
+                    <ModalCloseButton _hover={{ bg: "black", color: "white" }} />
                     <ModalBody>
                         <Stack direction={['column', 'row']} spacing={5} justify={'center'}>
                             <Card maxW='sm' border={'none'} boxShadow={'none'}>
@@ -56,14 +58,21 @@ const ModalStartHero = () => {
                                     />
                                 </CardBody>
                                 <CardFooter>
-                                    <Button 
-                                        as="a"
-                                        href='https://unipensiones.vercel.app/'
-                                        target={'_blank'}
-                                        variant='solid' 
-                                        colorScheme='blue' 
+                                    <Button
+                                        variant='solid'
+                                        colorScheme='blue'
                                         width={'full'}
-                                        rightIcon={<AiOutlineArrowRight />}>
+                                        rightIcon={<AiOutlineArrowRight />}
+                                        onClick={() =>
+                                            toast({
+                                                title: 'Estamos terminando de construirlo para tí',
+                                                description: "Estará habilitado el día 25 de Enero De 2024 🥳",
+                                                status: 'info',
+                                                duration: 9000,
+                                                isClosable: true,
+                                                variant: 'top-accent'
+                                            })
+                                        }>
                                         Soy Estudiante
                                     </Button>
                                 </CardFooter>
@@ -79,10 +88,13 @@ const ModalStartHero = () => {
                                     />
                                 </CardBody>
                                 <CardFooter>
-                                    <Button 
-                                        variant='solid' 
-                                        colorScheme='blue' 
-                                        width={'full'} 
+                                    <Button
+                                        as="a"
+                                        href={anfitrionesURL}
+                                        target={'_blank'}
+                                        variant='solid'
+                                        colorScheme='blue'
+                                        width={'full'}
                                         rightIcon={<AiOutlineArrowRight />}>
                                         Soy Anfitrión (a)
                                     </Button>
@@ -91,7 +103,7 @@ const ModalStartHero = () => {
                         </Stack>
                     </ModalBody>
                 </ModalContent>
-            </Modal>
+            </Modal >
         </>
     )
 }
